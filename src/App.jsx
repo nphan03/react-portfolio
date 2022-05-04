57b34acfa849ec2af936e958069661aa0b07bfbc
Alice Phan <nphan03.145@gmail.com>
2022-05-04 01:15:09 -0700

    done until nav

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import About from './components/about/About'; 
+import Skills from './components/skills/Skills';
+import Projects from './components/projects/Projects';
+import Education from './components/education/Education';
+import Contact from './components/contact/Contact';
+
+const App = () => {
+  return (
+    <>
+        <Header />
+        <Nav />
+        <About />
+        <Skills />
+        <Projects />
+        <Education />
+        <Contact />
+    </>
+  )
+};
+
+export default App;
