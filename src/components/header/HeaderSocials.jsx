3eadfc969a9921ea06d018948aa2a0e8162b65d0
Alice Phan <nphan03.145@gmail.com>
2022-05-07 13:09:26 -0700

    Update social links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import {BsGithub} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><BsGithub/></a>
+        <a href="https://www.linkedin.com/in/alice-phan-ab1a08198/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/nphan03" target="_blank"><BsGithub/></a>
     </div>
   )
 }
