d5464949c2af48eec8c09fd65f722395d42842d9
Alice Phan <nphan03.145@gmail.com>
2022-05-07 13:42:50 -0700

    Update icons on nav bar and add mouseover text

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import './nav.css'
 import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai';
 import {FaUserGraduate} from 'react-icons/fa'
-import {BiBook, BiMessageSquareDetail} from 'react-icons/bi'
-import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+import {MdWebStories} from 'react-icons/md';
+import {ImCog} from 'react-icons/im';
 
 const Nav = params => {
   //states passed from App
@@ -12,12 +13,13 @@ const Nav = params => {
 
   return (
     <nav>
-      <a href='#' onClick={()=> setActiveNav('#')} className={activeNav === "#" ? 'active' : ''}><AiOutlineHome/></a>
-      <a href='#about' onClick={()=> setActiveNav('#about')} className={activeNav === "#about" ? 'active' : ''}><AiOutlineUser/></a>
-      <a href='#skills' onClick={()=> setActiveNav('#skills')} className={activeNav === "#skills" ? 'active' : ''}><BiBook/></a>
-      <a href='#projects' onClick={()=> setActiveNav('#projects')} className={activeNav === "#projects" ? 'active' : ''}><RiServiceLine/></a>
-      <a href='#education' onClick={()=> setActiveNav('#education')} className={activeNav === "#education" ? 'active' : ''}><FaUserGraduate/></a>
-      <a href='#contact' onClick={()=> setActiveNav('#contact')} className={activeNav === "#contact" ? 'active' : ''}><BiMessageSquareDetail/></a>
+      {/*title='Home': create mouseover text */}
+      <a href='#' onClick={()=> setActiveNav('#')} className={activeNav === "#" ? 'active' : ''}><AiOutlineHome title='Home'/></a>
+      <a href='#about' onClick={()=> setActiveNav('#about')} className={activeNav === "#about" ? 'active' : ''}><AiOutlineUser title='About'/></a>
+      <a href='#skills' onClick={()=> setActiveNav('#skills')} className={activeNav === "#skills" ? 'active' : ''}><ImCog title='Skills'/></a>
+      <a href='#projects' onClick={()=> setActiveNav('#projects')} className={activeNav === "#projects" ? 'active' : ''}><MdWebStories title='Projects'/></a>
+      <a href='#education' onClick={()=> setActiveNav('#education')} className={activeNav === "#education" ? 'active' : ''}><FaUserGraduate title='Education'/></a>
+      <a href='#contact' onClick={()=> setActiveNav('#contact')} className={activeNav === "#contact" ? 'active' : ''}><BiMessageSquareDetail title='Contact'/></a>
     </nav>
   )
 }
